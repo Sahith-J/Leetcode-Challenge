e06d491be679e156914067ce05198c398ccbfdc9
Sahith-J <135148023+Sahith-J@users.noreply.github.com>
2024-01-31 10:51:35 +0530

    Update Solution.py

--- 0002. Add Two Numbers/Solution.py
+++ 0002. Add Two Numbers/Solution.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.next = next
 
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode()
